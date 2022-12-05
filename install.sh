f1c8514b934a626d792bba659698a5a41feb9c71
Stubbe <abs@clerk.io>
2022-12-05 10:11:12 +0100

    made fedora better

--- install.sh
+++ install.sh
@@ -49,146 +49,171 @@ case $pkgmn in
           exec $install_command cmake pkg-config libfreetype6-dev libfontconfig1-dev libxft-dev libx11-dev libxcb-xfixes0-dev libxkbcommon-dev python3 git-all curl zip tar git-all tmux zsh ripgrep neovim sshfs  < "/dev/null";
         fi
         echo "Installing JetBrainsMono Nerd Font"
-        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-        mkdir -p $HOME/.fonts
-        unzip JetBrainsMono.zip $HOME/.fonts
-        fc-cache -f
-        echo "Install Suckless Terminal"
-        rm -rf $HOME/.stubbe/builds/st
-        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st
-        cd $HOME/.stubbe/builds/st
-        cp $SOURCEWD/config/st/config.h $HOME/.stubbe/builds/st/config.h
-        sudo make clean install
-        cd $SOURCEWD
-        echo "export TERMINAL=st" >> $HOME/.profile
-        echo "Setting zsh as default shell"
-        chsh -s $(whereis zsh | cut -d' ' -f2)
-        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc
-        rm -f $HOME/.zshrc
-        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc
-        echo "Installing Aliases and Personal Functions"
-        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc
-        echo "Adding st_config and st_build to aliasrc"
-        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc
-        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc
-        rm -f $HOME/.aliasrc
-        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc
-        echo "Installing TMUX Plugin Manager"
-        rm -rf $HOME/.tmux/plugins/tpm
-        git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf
-        rm -f $HOME/.tmux.conf
-        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf
-        echo "Installing NvChad"
-        rm -rf $HOME/.local/share/nvim $HOME/.config/nvim
-        git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1
+        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip;
+        mkdir -p $HOME/.fonts;
+        unzip JetBrainsMono.zip $HOME/.fonts;
+        fc-cache -f;
+        echo "Install Suckless Terminal";
+        rm -rf $HOME/.stubbe/builds/st;
+        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st;
+        cd $HOME/.stubbe/builds/st;
+        cp $SOURCEWD/config/st/config.h $HOME/.stubbe/builds/st/config.h;
+        sudo make clean install;
+        cd $SOURCEWD;
+        echo "export TERMINAL=st" >> $HOME/.profile;
+        echo "Setting zsh as default shell";
+        chsh -s $(whereis zsh | cut -d' ' -f2);
+        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc;
+        rm -f $HOME/.zshrc;
+        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc;
+        echo "Installing Aliases and Personal Functions";
+        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc;
+        echo "Adding st_config and st_build to aliasrc";
+        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc;
+        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc;
+        rm -f $HOME/.aliasrc;
+        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc;
+        echo "Installing TMUX Plugin Manager";
+        rm -rf $HOME/.tmux/plugins/tpm;
+        git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm;
+        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf;
+        rm -f $HOME/.tmux.conf;
+        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf;
+        echo "Installing NvChad";
+        rm -rf $HOME/.local/share/nvim $HOME/.config/nvim;
+        git clone https://github.com/NvChad/NvChad $HOME/.config/nvim --depth 1 && nvim;
     ;;
     pacman)
-        exec $update_command
-        exec $upgrade_command
-        echo "Installing Dependencies"
-        exec $install_command --overwrite \* python-cairo
-        exec $install_command cmake freetype2 fontconfig pkg-config make python libxcb libxkbcommon zip tar git-all tmux zsh ripgrep neovim sshfs
-        echo "Installing JetBrainsMono Nerd Font"
-        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-        mkdir -p $HOME/.fonts
-        unzip JetBrainsMono.zip $HOME/.fonts
-        fc-cache -f -v
-        echo "Install Suckless Terminal"
-        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st
-        cd $HOME/.stubbe/builds/st
-        cp $SOURCEWD/config/st/config.h $SOURCEWD/config.h
-        sudo make clean install
-        echo "export TERMINAL=st" >> $HOME/.profile
-        echo "Setting zsh as default shell"
-        chsh -s $(whereis zsh | cut -d' ' -f2)
-        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc
-        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc
-        echo "Installing Aliases and Personal Functions"
-        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc
-        echo "Adding st_config and st_build to aliasrc"
-        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc
-        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc
-        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc
-        echo "Installing TMUX Plugin Manager"
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf
-        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf
-        echo "Installing NvChad"
-        rm -rf $HOME/.local/share/nvim
-        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+        echo -n "Have you installed dependencies already? (y)es / (n)o:";
+        read -r install_dep;
+        if [ $install_dep = 'n' || $install_dep = 'no' ]; then
+          echo "Installing Dependencies";
+          exec $upgrade_command --overwrite \* python-cairo;
+          exec $install_command cmake freetype2 fontconfig pkg-config make python libxcb libxkbcommon zip tar git-all tmux zsh ripgrep neovim sshfs;
+        fi
+        echo "Installing JetBrainsMono Nerd Font";
+        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip;
+        mkdir -p $HOME/.fonts;
+        unzip JetBrainsMono.zip $HOME/.fonts;
+        fc-cache -f;
+        echo "Install Suckless Terminal";
+        rm -rf $HOME/.stubbe/builds/st;
+        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st;
+        cd $HOME/.stubbe/builds/st;
+        cp $SOURCEWD/config/st/config.h $HOME/.stubbe/builds/st/config.h;
+        sudo make clean install;
+        cd $SOURCEWD;
+        echo "export TERMINAL=st" >> $HOME/.profile;
+        echo "Setting zsh as default shell";
+        chsh -s $(whereis zsh | cut -d' ' -f2);
+        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc;
+        rm -f $HOME/.zshrc;
+        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc;
+        echo "Installing Aliases and Personal Functions";
+        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc;
+        echo "Adding st_config and st_build to aliasrc";
+        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc;
+        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc;
+        rm -f $HOME/.aliasrc;
+        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc;
+        echo "Installing TMUX Plugin Manager";
+        rm -rf $HOME/.tmux/plugins/tpm;
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
+        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf;
+        rm -f $HOME/.tmux.conf;
+        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf;
+        echo "Installing NvChad";
+        rm -rf $HOME/.local/share/nvim $HOME/.config/nvim;
+        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim;
     ;;
     dnf)
-        exec $update_command
-        exec $upgrade_command
-        echo "Installing Dependencies"
-        exec $install_command cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ zip tar git-all tmux zsh ripgrep neovim sshfs
-        echo "Installing JetBrainsMono Nerd Font"
-        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-        mkdir -p $HOME/.fonts
-        unzip JetBrainsMono.zip $HOME/.fonts
-        fc-cache -f -v
-        echo "Install Suckless Terminal"
-        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st
-        cd $HOME/.stubbe/builds/st
-        cp $SOURCEWD/config/st/config.h $SOURCEWD/config.h
+        echo -n "Have you installed dependencies already? (y)es / (n)o:";
+        if [ $install_dep = 'n' || $install_dep = 'no' ]; then
+          exec $update_command;
+          echo "Installing Dependencies";
+          exec $install_command cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++ zip tar git-all tmux zsh ripgrep neovim sshfs;
+        fi
+        echo "Installing JetBrainsMono Nerd Font";
+        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip;
+        mkdir -p $HOME/.fonts;
+        unzip JetBrainsMono.zip $HOME/.fonts;
+        fc-cache -f;
+        echo "Install Suckless Terminal";
+        rm -rf $HOME/.stubbe/builds/st;
+        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st;
+        cd $HOME/.stubbe/builds/st;
+        cp $SOURCEWD/config/st/config.h $HOME/.stubbe/builds/st/config.h;
         sudo make clean install
-        echo "export TERMINAL=st" >> $HOME/.profile
-        echo "Setting zsh as default shell"
-        chsh -s $(whereis zsh | cut -d' ' -f2)
-        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc
-        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc
-        echo "Installing Aliases and Personal Functions"
-        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc
-        echo "Adding st_config and st_build to aliasrc"
-        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc
-        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc
-        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc
-        echo "Installing TMUX Plugin Manager"
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf
-        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf
-        echo "Installing NvChad"
-        rm -rf $HOME/.local/share/nvim
-        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+        cd $SOURCEWD;
+        echo "export TERMINAL=st" >> $HOME/.profile;
+        echo "Setting zsh as default shell";
+        chsh -s $(whereis zsh | cut -d' ' -f2);
+        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc;
+        rm -f $HOME/.zshrc;
+        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc;
+        echo "Installing Aliases and Personal Functions";
+        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc;
+        echo "Adding st_config and st_build to aliasrc";
+        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc;
+        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc;
+        rm -f $HOME/.aliasrc;
+        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc;
+        echo "Installing TMUX Plugin Manager";
+        rm -rf $HOME/.tmux/plugins/tpm;
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
+        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf;
+        rm -f $HOME/.tmux.conf;
+        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf;
+        echo "Installing NvChad";
+        rm -rf $HOME/.local/share/nvim $HOME/.config/nvim;
+        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim;
     ;;
     brew)
         which -s brew
         if [[ $? != 0 ]] ; then
             curl -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         fi
-        exec $update_command
-        exec $upgrade_command
-        echo "Installing Dependencies"
-        exec $install_command tmux zsh neovim ripgrep git-archive-all
-        echo "Installing JetBrainsMono Nerd Font"
-        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
-        mkdir -p $HOME/.fonts
-        unzip JetBrainsMono.zip $HOME/.fonts
-        fc-cache -f -v
-        echo "Install Suckless Terminal"
-        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st
-        cd $HOME/.stubbe/builds/st
-        cp $SOURCEWD/config/st/config.h $SOURCEWD/config.h
-        sudo make clean install
-        echo "export TERMINAL=st" >> $HOME/.profile
-        echo "Setting zsh as default shell"
-        chsh -s $(whereis zsh | cut -d' ' -f2)
-        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc
-        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc
-        echo "Installing Aliases and Personal Functions"
-        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc
-        echo "Adding st_config and st_build to aliasrc"
-        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc
-        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc
-        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc
+        echo -n "Have you installed dependencies already? (y)es / (n)o:";
+        if [ $install_dep = 'n' || $install_dep = 'no' ]; then
+          exec $update_command;
+          exec $upgrade_command;
+          echo "Installing Dependencies";
+          exec $install_command tmux zsh neovim ripgrep git-archive-all;
+        fi
+        echo "Installing JetBrainsMono Nerd Font";
+        curl -O https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip;
+        mkdir -p $HOME/.fonts;
+        unzip JetBrainsMono.zip $HOME/.fonts;
+        fc-cache -f;
+        echo "Install Suckless Terminal";
+        rm -rf $HOME/.stubbe/builds/st;
+        git clone https://git.suckless.org/st $HOME/.stubbe/builds/st;
+        cd $HOME/.stubbe/builds/st;
+        cp $SOURCEWD/config/st/config.h $HOME/.stubbe/builds/st/config.h;
+        sudo make clean install;
+        cd $SOURCEWD;
+        echo "export TERMINAL=st" >> $HOME/.profile;
+        echo "Setting zsh as default shell";
+        chsh -s $(whereis zsh | cut -d' ' -f2);
+        cp $SOURCEWD/config/zsh/.zshrc $HOME/.stubbe/config/.zshrc;
+        rm -f $HOME/.zshrc;
+        ln -s $HOME/.stubbe/config/.zshrc $HOME/.zshrc;
+        echo "Installing Aliases and Personal Functions";
+        cp $SOURCEWD/config/aliases/.aliasrc $HOME/.stubbe/config/.aliasrc;
+        echo "Adding st_config and st_build to aliasrc";
+        echo "alias st_config='$EDITOR $HOME/.stubbe/builds/st/config.h'" >> $HOME/.stubbe/config/.aliasrc;
+        echo "alias st_build='cd $HOME/.stubbe/builds/st && sudo make clean install && cd $HOME && echo reopen terminal!'" >> $HOME/.stubbe/config/.aliasrc;
+        rm -f $HOME/.aliasrc;
+        ln -s $HOME/.stubbe/config/.aliasrc $HOME/.aliasrc;
         echo "Installing TMUX Plugin Manager"
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf
-        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf
-        echo "Installing NvChad"
-        rm -rf $HOME/.local/share/nvim
-        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+        rm -rf $HOME/.tmux/plugins/tpm;
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
+        cp $SOURCEWD/config/tmux/.tmux.conf $HOME/.stubbe/config/.tmux.conf;
+        rm -f $HOME/.tmux.conf;
+        ln -s $HOME/.stubbe/config/.tmux.conf $HOME/.tmux.conf;
+        echo "Installing NvChad";
+        rm -rf $HOME/.local/share/nvim $HOME/.config/nvim;
+        git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim;
     ;;
 esac
 
